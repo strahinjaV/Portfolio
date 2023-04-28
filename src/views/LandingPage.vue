68ba58e02d62be2c33284633d00af8deb9a20a5e
strahinjaPC <strajo98@gmail.com>
2023-04-27 23:49:13 -0400

    part of welcome text

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,7 +1,32 @@
 <template>
-  <div></div>
+  <section id="welcomeSection">
+    <div class="row">
+      <div class="col-lg-12 welcomeText">
+        <h1>Strahinja Vidovic</h1>
+        <p>FRONT END SOFTWARE ENGINEER</p>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script></script>
+<style scoped>
+#welcomeSection {
+  background-image: url("../assets/nightSky.jpg");
+  height: 40%;
+}
+.welcomeText {
+  text-align: center;
+}
 
-<style scoped></style>
+.welcomeText h1 {
+  font-family: "Playfair Display", serif;
+  font-weight: 700;
+  color: #fffcfb;
+}
+
+.welcomeText p {
+  font-family: "Montserrat", sans-serif;
+  font-weight: 400;
+  color: #fffcfb;
+}
+</style>
